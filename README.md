6ce88ea7892a7144914e3390e6376e8c1a9af6cf
akero11 <luisangelw3@outlook.com>
2021-02-27 17:05:23 -0600

    create and define basic structure for main.py

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # kaprekar_sosf
+
+## Objective
+
+Code [Kaprekar's routine](https://en.wikipedia.org/wiki/Kaprekar%27s_routine) for four-digit numbers.
+
+I'm doing nothing serious, this is just for fun.
---
+++ main.py
@@ -0,0 +1,22 @@
+def extremes(number):
+    sorted_ = sorted(number)
+    asc = ''.join(sorted_[::-1])
+    des = ''.join(sorted_)
+    return(asc, des)
+
+
+def fill(number, side):
+    digits = len(number)
+    ...
+
+
+def kaprekar_routine(number):
+    ...
+
+
+def run():
+    ...
+
+
+if __name__ == '__main__':
+    run()
