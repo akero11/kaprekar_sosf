0d96991dd578c5c1f853913321a8d6876a15458e
akero11 <luisangelw3@outlook.com>
2021-02-27 19:03:05 -0600

    develop several functions in main.py

--- main.py
+++ main.py
@@ -5,13 +5,23 @@ def extremes(number):
     return(asc, des)
 
 
-def fill(number, side):
+def fill(number, side = 'left'):
     digits = len(number)
-    ...
+    zeros = '0' * (4 - digits)
 
+    full_number = (zeros + number) if side == 'left' else (number + zeros)
+
+    return(full_number)
 
 def kaprekar_routine(number):
-    ...
+    
+    steps = 0
+
+    if number != 6174:
+        str_number = str(number)
+        full_str_number = fill(str_number) if len(str_number)<4 else str_number
+    else:
+        return(steps)
 
 
 def run():
